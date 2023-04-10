656194021d10998906d2b02ec04fa1cf4d0ae6bd
Belfegorrr <pelichek.iago@gmail.com>
2023-04-10 15:44:48 -0300

    estilizar seção de projetos e responsividade

---
+++ src/components/Projeto/index.tsx
@@ -0,0 +1,13 @@
+import Paragrafo from '../Paragrafo'
+import Titulo from '../Titulo'
+import { Card, LinkBotao } from './styles'
+
+const Projeto = () => (
+  <Card>
+    <Titulo>Projeto Lista de tarefas</Titulo>
+    <Paragrafo tipo="secundario">Lista de tarefas feita com VueJS</Paragrafo>
+    <LinkBotao>Vizualizar</LinkBotao>
+  </Card>
+)
+
+export default Projeto
--- src/style.ts
+++ src/style.ts
@@ -6,10 +6,15 @@ const EstiloGlobal = createGlobalStyle`
   padding: 0;
   box-sizing: border-box;
   font-family: 'Inter', sans-serif;
+  list-style: none;
 }
 
 body {
   padding-top: 80px;
+
+  @media (max-width: 768px) {
+    padding-top: 16px
+  }
 }
 `
 
@@ -22,6 +27,12 @@ export const Container = styled.div`
   display: grid;
   grid-template-columns: 128px auto;
   column-gap: 56px;
+
+  @media (max-width: 768px) {
+    max-width: 80%;
+    display: block;
+  }
+
   img {
     max-width: 100%;
   }
