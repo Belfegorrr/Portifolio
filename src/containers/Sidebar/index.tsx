6070bf7472e57b9384057874b4382b1434afb4ca
Belfegorrr <pelichek.iago@gmail.com>
2023-04-10 16:45:59 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import { BotaoTema, Descricao, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/Belfegorrr.png" />
-    <Titulo fontSize={20}>Iago Pelichek Fernandes</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Iago Pelichek Fernandes</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        Belfegorrr
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Desenvolvedor FullStack Java
+      </Descricao>
+      <BotaoTema>Trocar Tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
