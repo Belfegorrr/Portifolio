dc1f092a581693764a0b7308fb7ebafde53e0a8b
Belfegorrr <pelichek.iago@gmail.com>
2023-04-10 20:25:49 -0300

    adicionar responsividade

--- src/containers/Sidebar/styles.ts
+++ src/containers/Sidebar/styles.ts
@@ -21,4 +21,9 @@ export const SidebarContainer = styled.div`
   position: sticky;
   top: 80px;
   left: 0;
+
+  @media (max-width: 768px) {
+    margin-bottom: 40px;
+    text-align: center;
+  }
 `
